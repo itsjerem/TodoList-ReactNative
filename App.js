ef9204adc6ca3cdbca788b04cb8412d36070bd2b
itsjerem <jeremytroccaz@gmail.com>
2024-02-14 14:17:54 +0100

    #STEP 2: Ajouter la fonctionnalité d'ajout d'objectifs et d'affichage dans une liste

--- App.js
+++ App.js
@@ -1,27 +1,73 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
+  const [goals, setGoals] = useState([]);
+  const [enteredGoal, setEnteredGoal] = useState("");
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setGoals((currentGoals) => [...currentGoals, enteredGoal]);
+    setEnteredGoal("");
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Nouvel objectif"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="Ajouter" onPress={addGoalHandler} />
+      </View>
+      <ScrollView>
+        {goals.map((goal, index) => (
+          <View key={index} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingVertical: 75,
+    paddingHorizontal: 30,
   },
-  text: {
-    color: "red",
-    fontWeight: "bold",
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "#gray",
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "#white",
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#white",
+    borderColor: "#gray",
+    borderWidth: 1,
+    borderRadius: 5,
   },
 });
